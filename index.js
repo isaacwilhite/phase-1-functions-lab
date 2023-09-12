bc7c4803db54a6dcc276758ad6a0e143f4b4993d
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-09-12 09:41:31 -0600

    solution

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNumber) {
+if (blockNumber > 42) {
+    return blockNumber - 42
+}
+return Math.abs(blockNumber - 42)
+}
+function distanceFromHqInFeet(blockNumber){
+return distanceFromHqInBlocks(blockNumber) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264)
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
